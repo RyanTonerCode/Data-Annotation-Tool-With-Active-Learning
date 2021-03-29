90c4d6ad29601e805598c0a0b11589d38a648da0
Ryan Toner <ryan.toner@student.fairfield.edu>
2021-03-29 15:08:26 -0400

    refactoring, created method to train the existing model

--- application/routes/intelligents.py
+++ application/routes/intelligents.py
@@ -30,10 +30,7 @@ def create_model(text_encoder, NUM_CLASSES):
 
 
 def get_initial_labelled_dataset(X_Data, Y_Data, num_samples_per_class=10):
-    X = []
-    y = []
-    X_pooled = []
-    y_pooled = []
+    X, Y, X_pooled, Y_pooled = [], [], [], []
     labelled_idx = []
     counter_dict = {}
 
@@ -47,14 +44,13 @@ def get_initial_labelled_dataset(X_Data, Y_Data, num_samples_per_class=10):
         else:
             counter_dict[index] = 1
         X.append(X_Data[idx])
-        y.append(target)
+        Y.append(target)
         labelled_idx.append(idx)
         
     X_pooled = np.delete(X_Data, labelled_idx, axis=0)
-    y_pooled = np.delete(Y_Data, labelled_idx, axis=0)
+    Y_pooled = np.delete(Y_Data, labelled_idx, axis=0)
 
-    return np.asarray(X), np.asarray(y), X_pooled, y_pooled
-
+    return np.asarray(X), np.asarray(Y), X_pooled, Y_pooled
 
 
 def max_entropy_acquisition(model, X_pooled): 
@@ -67,32 +63,27 @@ def max_entropy_acquisition(model, X_pooled):
 
 
 
-def manage_data(X, y, X_pooled, y_pooled, idx):
+def manage_data(X, Y, X_pooled, Y_pooled, idx):
     pool_mask = np.ones(len(X_pooled), dtype=bool)
     pool_mask[idx] = False
     pool_mask_2 = np.zeros(len(X_pooled), dtype=bool)
     pool_mask_2[idx] = True
     new_training = X_pooled[pool_mask_2]
-    new_label = y_pooled[pool_mask_2]
+    new_label = Y_pooled[pool_mask_2]
     X_pooled = X_pooled[pool_mask]
-    y_pooled = y_pooled[pool_mask]
+    Y_pooled = Y_pooled[pool_mask]
     X = np.concatenate([X, new_training])
-    y = np.concatenate([y, new_label])
-    return X, y, X_pooled, y_pooled
+    Y = np.concatenate([Y, new_label])
+    return X, Y, X_pooled, Y_pooled
 
 
-
-######## WHEN DOES OLD MODEL GET PASSED TO NEW?
-def initialize_model(user_data):
-    NUM_CLASSES = len(user_data["tag_data"]["tags"])
-
+def extractUserData(user_data):
     X_Data, Y_Data = [], [] #here, load the training data
     for sentence_index, sentence in enumerate(user_data["sentences"]):
         for word_index, word in enumerate(sentence.split()):
             tag_index = int(user_data["sentence_tags"][sentence_index][word_index]) #get tag index ID number from current word
             if tag_index != 0: #retrieve tag data only if there's a tag
-                print("TAG INDEX: ", tag_index)
-                # continue
+                #print("TAG INDEX: ", tag_index)
                 #tag_info = user_data["tag_data"]["tags"][tag_index]["name"] 
                 X_Data.append(word)
                 #one hot encode the classes
@@ -101,13 +92,18 @@ def initialize_model(user_data):
                 Y_Data.append(one_hot)
 
     #convert X_Data to numpy array
-    X_Data_np = np.array(X_Data)
-    Y_Data_np = np.array(Y_Data)
+    return (np.array(X_Data), np.array(Y_Data))
+
+
+def initialize_model(user_data):
+    NUM_CLASSES = len(user_data["tag_data"]["tags"])
+
+    X_Data, Y_Data = extractUserData(user_data)
 
     #set the number of values to sqrt of the length of the data
-    num_samples_per_class = int(math.sqrt(len(X_Data_np)))
+    num_samples_per_class = int(math.sqrt(len(X_Data)))
 
-    X, y, X_pooled, y_pooled = get_initial_labelled_dataset(X_Data_np, Y_Data_np, num_samples_per_class)
+    X, Y, X_pooled, Y_pooled = get_initial_labelled_dataset(X_Data, Y_Data, num_samples_per_class)
 
     VOCAB_SIZE = 2000
 
@@ -121,17 +117,32 @@ def initialize_model(user_data):
         text_encoder.adapt(X)
 
         model = create_model(text_encoder, NUM_CLASSES)
-        model.fit(X, y, epochs=1, verbose=1)
+        model.fit(X, Y, epochs=1, verbose=1)
         uncertain_idx, entropy_avg = max_entropy_acquisition(model, X_pooled)
         print('Average Entropy: {}'.format(entropy_avg))
         
-        X, y, X_pooled, y_pooled = manage_data(X, y, X_pooled, y_pooled, uncertain_idx)
+        X, Y, X_pooled, Y_pooled = manage_data(X, Y, X_pooled, Y_pooled, uncertain_idx)
         print('Iteration Done: {}'.format(i+1))
 
     #return the model back
     return model
 
 
+def train_existing_model(user_data):
+    X_Data, Y_Data = extractUserData(user_data)
+
+    #set the number of values to sqrt of the length of the data
+    num_samples_per_class = int(math.sqrt(len(X_Data)))
+
+    X, Y, X_pooled, Y_pooled = get_initial_labelled_dataset(X_Data, Y_Data, num_samples_per_class)
+
+    #load the existing model
+    model = load_model(user_data)
+
+    model.fit(X, Y, epochs=3, verbose=1)
+
+    #return the model back
+    return model
 
 def get_path(sub):
     return os.path.join(os.getcwd(), "application", "data", "ai", sub)
@@ -154,16 +165,6 @@ def load_model(user_data, model_name=None):
 def run_model(user_data, test_sentences):
 
     model = load_model(user_data)
-    # model = app.config["ai_model"] if app.config["ai_model"] else tf.keras.models.load_model(get_path(model_name)) if os.path.exists(get_path(model_name)) else None
-    # if app.config["ai_model"]:
-    #     model = app.config["ai_model"]
-    # else: 
-    #     model_path = get_path(user_data["model_name"])
-    #     if os.path.exists(model_path):#load the model if it exists but is not in memory
-    #         model = tf.keras.models.load_model(model_path)
-    #     else: 
-    #         # return user_data # ERROR no model how did this happen!
-    #         return initialize_model(user_data["model_name"])
 
     #get a prediction for every word in the selected sentences
     new_user_data = user_data
@@ -190,3 +191,4 @@ def save_model(model, model_name):
     print("Model Saving started")
     model.save(model_path)
     print("Complete")
+    
